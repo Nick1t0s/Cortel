b017f7db3303bd25fab24e7470b31695b0ff77e1
Nik <Nick1t0s@yandex.ru>
2025-03-22 12:34:35 +0300

    Исправление ошибок

--- prepare.sql
+++ prepare.sql
@@ -1,18 +1,19 @@
-CREATE TABLE users(
+CREATE TABLE IF NOT EXISTS users(
     user_id SERIAL8 PRIMARY KEY,
-    username text PRIMARY KEY NOT NULL,
+    username text UNIQUE NOT NULL,
     password_hash VARCHAR(255) NOT NULL,
     avatar_path VARCHAR(255) NOT NULL,
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-    last_login TIMESTAMPTZ NOT NULL,
+    last_login TIMESTAMPTZ NOT NULL
 );
 
-CREATE TABLE chats(
+CREATE TABLE IF NOT EXISTS chats(
     chat_id SERIAL8 PRIMARY KEY,
-    users_id SERIAL8[],
-    admins_id SERIAL8[],
+    users_id int8[],
+    admins_id int8[],
     open bool DEFAULT FALSE,
     users_Invite bool DEFAULT FALSE,
     avatar_path VARCHAR(255),
-    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    created_by SERIAL8
 )
